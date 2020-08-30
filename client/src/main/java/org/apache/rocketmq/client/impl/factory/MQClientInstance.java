773a447addf134e6a619a33fbc55e822566080de
arthas <jlj_vip98@qq.com>
2020-08-30 18:42:46 +0800

    源码分析

--- client/src/main/java/org/apache/rocketmq/client/impl/factory/MQClientInstance.java
+++ client/src/main/java/org/apache/rocketmq/client/impl/factory/MQClientInstance.java
@@ -91,7 +91,7 @@ public class MQClientInstance {
     private final int instanceIndex;
     private final String clientId;
     private final long bootTimestamp = System.currentTimeMillis();
-    private final ConcurrentMap<String/* group */, MQProducerInner> producerTable = new ConcurrentHashMap<String, MQProducerInner>();
+    private final ConcurrentMap<String/* group */, MQProducerInner>   producerTable = new ConcurrentHashMap<String, MQProducerInner>();
     private final ConcurrentMap<String/* group */, MQConsumerInner> consumerTable = new ConcurrentHashMap<String, MQConsumerInner>();
     private final ConcurrentMap<String/* group */, MQAdminExtInner> adminExtTable = new ConcurrentHashMap<String, MQAdminExtInner>();
     private final NettyClientConfig nettyClientConfig;
